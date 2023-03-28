42eda529a768e0dd13f47dd1e9e0fe590750a904
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-27 23:16:02 -0500

    First Lesson about variables in Kotlin Done

---
+++ Atomic_Kotlin/src/main/kotlin/Programming_Basics/EmptyProgram.kt
@@ -0,0 +1,23 @@
+package Programming_Basics// HelloWorld/EmptyProgram.kt
+
+fun main(){
+    // Program code here ...
+    println("Hello, world!")
+
+    val whole = 11
+    val fractional = 1.4
+    val words = "Twas Brilling"
+
+    println(whole)
+    println(fractional)
+    println(words)
+
+
+    var sum = 1
+    sum += 2
+    sum += 3
+    println(sum)
+
+
+}
+
